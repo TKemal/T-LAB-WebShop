a7a38f7e221c1ee95c36d8edd8c55d437bfe8b44
Kemal Tihic <tihickemal@gmail.com>
2020-12-16 18:15:05 +0100

    16/12 II

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
 import product from '../products'
 
 const HomeScreen = () => {
@@ -10,7 +11,7 @@ const HomeScreen = () => {
       <Row>
         {product.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
