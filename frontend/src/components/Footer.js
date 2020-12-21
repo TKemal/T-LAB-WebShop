a5d141bb3bf2d297b1d3120e6c99ec0080bf9201
Kemal Tihic <tihickemal@gmail.com>
2020-12-21 22:00:48 +0100

    21/12

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Made with ❤ in T-LAB BiH</Col>
+          <Col className='text-center py-3'>
+            Copyright ©2020 All rights reserved | Made with ❤ in T-LAB BiH
+          </Col>
         </Row>
       </Container>
     </footer>
