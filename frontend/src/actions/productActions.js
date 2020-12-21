cfdc2a058e0c8a7b302d7dc446dfa3b1aeff305f
Kemal Tihic <tihickemal@gmail.com>
2020-12-21 20:51:50 +0100

    21/12

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispach) => {
@@ -18,6 +21,27 @@ export const listProducts = () => async (dispach) => {
   } catch (error) {
     dispach({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const listProductDetails = (id) => async (dispach) => {
+  try {
+    dispach({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispach({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispach({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
