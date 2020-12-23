6151dafbc54ee12d049b521dffe74e96ed654c8f
Kemal Tihic <tihickemal@gmail.com>
2020-12-23 22:52:14 +0100

    23/12

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,4 +23,55 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or password!')
   }
 })
-export { authUser }
+
+//@Desc Register a new user
+//@Route POST /api/users
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExsist = await User.findOne({ email })
+
+  if (userExsist) {
+    res.status(400)
+    throw new Error('User already exsist')
+  }
+  const user = User.create({
+    name,
+    email,
+    password,
+  })
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data!')
+  }
+})
+
+//@Desc Get user profile
+//@Route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found!')
+  }
+})
+
+export { authUser, registerUser, getUserProfile }
