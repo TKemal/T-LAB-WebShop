2fb67bf642376b89660f154b84f3d4a2a45c7850
Kemal Tihic <tihickemal@gmail.com>
2021-01-09 17:24:01 +0100

    9/1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ const App = () => {
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
